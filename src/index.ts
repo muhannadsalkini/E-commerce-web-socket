cd8fb3cee3b0d048d9862422b0d6c65890a8cc0e
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2024-10-01 13:04:28 +0300

    Added TODO comments

--- src/index.ts
+++ src/index.ts
@@ -27,3 +27,17 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err);
   res.status(500).json({ message: "Internal Server Error" });
 });
+
+// TODO: Add an error handle middleware
+// TODO: Add the order functionality
+// TODO: Add the ports functionality for the live orders
+// TODO: Add roles to the user
+// TODO: Add user basket functionality
+// TODO: Add integration for payment gateways (izyco or param iframe)
+// TODO: Add email verification and password reset functionality
+// TODO: Add user activity logs (login attempts, actions, etc.)
+// TODO: Add sorting and filtering for products and orders
+// TODO: Add address management for users (shipping addresses)
+// TODO: Add review and rating system for products
+// TODO: Add wishlist functionality for users
+// TODO: Implement file upload validation (image size, file type checks)
